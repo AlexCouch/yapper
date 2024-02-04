da72ce92d81b077a1a6efe0724073aa65989f30a
alexsofsafe <alcouch65@gmail.com>
2024-02-04 15:00:39 -0800

    Templates now have child templates; started on home page layout

--- cmd/templates/template.go
+++ cmd/templates/template.go
@@ -10,29 +10,43 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type Template struct{
-    templates map[string]*template.Template
+type Template struct {
+	templates map[string]*template.Template
 }
 
-func NewTemplate() *Template{
-    return &Template{
-        templates: map[string]*template.Template{},
-    }
+func NewTemplate() *Template {
+	return &Template{
+		templates: map[string]*template.Template{},
+	}
 }
 
-func (t *Template) AddTemplate(name string) {
-    path := filepath.Join("public/views/", name + ".html")
-    t.templates[name] = template.Must(template.ParseFiles("public/views/base.html", "public/views/topbar.html", path))
+func (t *Template) normalizePaths(paths []string) []string {
+	normals := []string{}
+	for _, path := range paths {
+		normal := filepath.Join("public/views/", path+".html")
+		normals = append(normals, normal)
+	}
+	return normals
 }
 
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error{
-    templ, ok := t.templates[name]
-    if !ok{
-        return errors.New("Template " + name + " does not exist in template map!")
-    }
-    err := templ.ExecuteTemplate(w, "base", data)
-    if err != nil{
-        return c.String(http.StatusOK, err.Error())
-    }
-    return nil
+func (t *Template) AddTemplate(name string, children ...string) {
+	path := filepath.Join("public/views/", name+".html")
+	children = t.normalizePaths(children)
+	paths := []string{}
+	paths = append(paths, path)
+	paths = append(paths, children...)
+	paths = append(paths, "public/views/base.html")
+	t.templates[name] = template.Must(template.ParseFiles(paths...))
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	templ, ok := t.templates[name]
+	if !ok {
+		return errors.New("Template " + name + " does not exist in template map!")
+	}
+	err := templ.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		return c.String(http.StatusOK, err.Error())
+	}
+	return nil
 }
--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	//"github.com/labstack/echo-jwt"
+
 	"github.com/golang-jwt/jwt/v5"
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
@@ -11,46 +12,45 @@ import (
 	"yapper.com/m/yapper/cmd/templates"
 )
 
-func main(){
-    e := echo.New()
-    e.Static("/", "static")
+func main() {
+	e := echo.New()
+	e.Static("/", "static")
 
-    e.Use(middleware.Logger())
-    //We need to customize the JWT mw to redirect the user to the login page 
-    // if the token is expired or simply invalid
-    //e.Use(echojwt.JWT([]byte("secret")))
+	e.Use(middleware.Logger())
+	//We need to customize the JWT mw to redirect the user to the login page
+	// if the token is expired or simply invalid
+	//e.Use(echojwt.JWT([]byte("secret")))
 
-    templs := templates.NewTemplate()
-    templs.AddTemplate("index")
-    templs.AddTemplate("signup")
-    templs.AddTemplate("signin")
-    templs.AddTemplate("profile")
-    templs.AddTemplate("edit_profile")
-    templs.AddTemplate("topbar")
-    e.Renderer = templs
-    
-    e.GET("/", handlers.Home)
-    e.GET("/user/:id", handlers.UserProfile)
-    e.GET("/register", handlers.GotoRegister)
-    e.POST("/register", handlers.Register)
-    e.GET("/signin", handlers.GotoSignin)
-    e.POST("/signin", handlers.Signin)
+	templs := templates.NewTemplate()
+	templs.AddTemplate("index", "topbar", "sidebar", "extra", "yap")
+	templs.AddTemplate("signup", "topbar")
+	templs.AddTemplate("signin", "topbar")
+	templs.AddTemplate("profile", "topbar")
+	templs.AddTemplate("edit_profile", "topbar")
+	e.Renderer = templs
 
-    edit := e.Group("edit")
-    config := echojwt.Config{
-        NewClaimsFunc: func(c echo.Context) jwt.Claims{
-            return new(auth.CustomClaim)
-        },
-        SigningKey: []byte("secret"),
-        TokenLookup: "cookie:user",
-        ErrorHandler: func(c echo.Context, err error) error {
-            println(err.Error())
-            return err
-        },
-    }
-    edit.Use(echojwt.WithConfig(config))
+	e.GET("/", handlers.Home)
+	e.GET("/user/:id", handlers.UserProfile)
+	e.GET("/register", handlers.GotoRegister)
+	e.POST("/register", handlers.Register)
+	e.GET("/signin", handlers.GotoSignin)
+	e.POST("/signin", handlers.Signin)
 
-    edit.GET("/:id", handlers.EditProfile)
-    edit.POST("/:id", handlers.SaveProfileChanges)
-    e.Logger.Fatal(e.Start(":1323"))
+	edit := e.Group("edit")
+	config := echojwt.Config{
+		NewClaimsFunc: func(c echo.Context) jwt.Claims {
+			return new(auth.CustomClaim)
+		},
+		SigningKey:  []byte("secret"),
+		TokenLookup: "cookie:user",
+		ErrorHandler: func(c echo.Context, err error) error {
+			println(err.Error())
+			return err
+		},
+	}
+	edit.Use(echojwt.WithConfig(config))
+
+	edit.GET("/:id", handlers.EditProfile)
+	edit.POST("/:id", handlers.SaveProfileChanges)
+	e.Logger.Fatal(e.Start(":1323"))
 }
