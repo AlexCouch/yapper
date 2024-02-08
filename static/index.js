a781d634f43635551209f4cd5c82ad52d5ad8264
alexsofsafe <alcouch65@gmail.com>
2024-02-07 20:01:35 -0800

    Added favicon and improved look and feel of yap audio player

--- static/index.js
+++ static/index.js
@@ -1,8 +1,12 @@
 import WaveSurfer from 'https://unpkg.com/wavesurfer.js@7/dist/wavesurfer.esm.js'
+
 const container = document.getElementById('waveformContainer')
 const rect = container.getBoundingClientRect()
 const width = rect.width
 const height = rect.height
+
+const play_fa = 'fa-play'
+const pause_fa = 'fa-pause'
 
 const wavesurfer = WaveSurfer.create({
     container: "#waveform",
@@ -16,8 +20,57 @@ const wavesurfer = WaveSurfer.create({
     barHeight: 1.1,
     barRadius: 12,
     barGap: 2,
+    dragToSeek: true,
 })
 
-wavesurfer.on('interaction', ()=>{
+function setPauseButton(){
+    let yap_play = $('#yap_play')
+    yap_play.removeClass(play_fa)
+    yap_play.addClass(pause_fa)
+}
+
+function setPlayButton(){
+    let yap_play = $('#yap_play')
+    yap_play.removeClass(pause_fa)
+    yap_play.addClass(play_fa)
+}
+
+function stop(){
+    wavesurfer.pause()
+    wavesurfer.seekTo(0)
+    setPlayButton()
+}
+
+function pause(){
+    console.log("Pausing...")
+    wavesurfer.pause()
+    setPlayButton()
+}
+
+function play(){
+    console.log("Playing...")
     wavesurfer.play()
+    setPauseButton()
+}
+
+$(document).on('click', function(e){
+    if(!$(event.target).closest('#yap').length){
+        stop()
+    }
+})
+
+wavesurfer.on('ready', function(){
+    setPlayButton()
+})
+
+wavesurfer.on('finish', function(){
+    stop()
+})
+
+$('#play_button').on('click', function(e){
+    if(wavesurfer.isPlaying()){
+        pause()
+    }else{
+        play()
+    }
 })
