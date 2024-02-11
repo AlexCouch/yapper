4e190cb915cd5693ecc41d63e679681b3899cb5d
alexsofsafe <alcouch65@gmail.com>
2024-02-10 22:06:04 -0800

    Renamed content.templ to sidebar/templ

--- cmd/views/sidebar_templ.go
+++ cmd/views/sidebar_templ.go
@@ -30,7 +30,7 @@ func Sidebar() templ.Component {
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs("@alexcouch")
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `cmd/views/content.templ`, Line: 30, Col: 22}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `cmd/views/sidebar.templ`, Line: 30, Col: 22}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
